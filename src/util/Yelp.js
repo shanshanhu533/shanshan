01564390184c7529c653b707cec5a01579a0d250
shanshan <jennyhu533533@gmail.com>
2017-10-31 22:00:29 -0600

    ravenous part 4

---
+++ src/util/Yelp.js
@@ -0,0 +1,42 @@
+const clientId='UNweIyUVO3Ln9SDPsCAXlA';
+const secret='O9kHqXtv0Ah62K4IwS9yRgpZr1XU9PKVSxU3H4cbnGbyPjSqw6mhlIU2nxQG97su';
+let accessToken='';
+const Yelp={
+  getAccessToken(){
+    if(accessToken){
+      return new Promise(resolve =>resolve(accessToken));
+    }
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+          {method:'POST'}).then(response=>
+        {
+        return response.json()
+        }).then(jsonResponse => accessToken=jsonResponse.access_token);
+  },
+
+  search(term,location,sortBy){
+    return Yelp.getAccessToken().then(()=>{
+      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+          headers:{Authorization:`Bearer ${accessToken}`}
+        }).then(response =>{
+        return response.json()}).then(jsonResponse => {
+          if(jsonResponse.businesses){
+            return jsonResponse.businesses.map(business =>{
+              return {id:business.id,
+                imageSrc: business.image_url,
+                name:business.name,
+                address:business.location.address1,
+                city:business.location.city,
+                state:business.location.state,
+                zipCode:business.location.zip_code,
+                category:business.categories[0].title,
+                rating:business.rating,
+                reviewCount:business.review_count
+              };
+            });
+          }
+      });
+    })
+  }
+};
+export default Yelp;
